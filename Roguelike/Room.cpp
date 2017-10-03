fcdc720e2a3d745b9e391ecadd0edcb66c083f72
Bryan <bryanschreuder91@gmail.com>
2017-10-03 15:31:13 +0200

    Small changes to room

--- Roguelike/Room.cpp
+++ Roguelike/Room.cpp
@@ -4,7 +4,6 @@
 Room::Room()
 {
 
-	enemies = new std::vector<Unit>;
 
 
 }
@@ -12,25 +11,38 @@ Room::Room()
 
 Room::~Room()
 {
-	delete enemies;
 
 }
 
 
-void Room::addEnemy(Enemy e) {
-	enemies->push_back(e);
+void Room::addEnemy(Enemy* e) {
+	enemies.push_back(e);
+	//e->setCurrentRoom(this);
 }
 
-void Room::removeEnemy(Enemy e) {
 
-}
-
-void Room::addNeighbor(std::string s, Room* r) {
-	if (neighbors[s] == NULL) {
-		neighbors[s] = r;
+void Room::addAdjacentRoom(Direction d, Room* r) {
+	if (d == null) {
+		for (std::map<Direction, Room*>::iterator it = oldRooms.begin(); it != oldRooms.end(); ++it) {
+			if (it->second == r) {
+				adjacentRooms.insert(std::pair<Direction, Room*>(it->first, r));
+			}
+		}
+	} else {
+		adjacentRooms.insert(std::pair<Direction, Room*>(d, r));
 	}
 }
 
-void Room::removeNeighbor(std::string) {
+void Room::addCollapsedRoom(Room* r) {
+	for (std::map<Direction, Room*>::iterator it = oldRooms.begin(); it != oldRooms.end(); ++it) {
+		if (it->second == r) {
+			collapsedRooms.insert(std::pair<Direction, Room*>(it->first, r));
+		}
+	}
 
+}
+
+void Room::clearRooms() {
+	oldRooms = adjacentRooms;
+	adjacentRooms.clear();
 }
--- Roguelike/Room.h
+++ Roguelike/Room.h
@@ -5,18 +5,47 @@
 #include "Unit.h"
 #include "Enemy.h"
 
+enum Direction
+{
+	null, NORTH, EAST, SOUTH, WEST
+};
+
 class Room
 {
 public:
 	Room();
 	~Room();
-	std::map<std::string, Room*> neighbors; //hashmap
-	std::vector<Unit>*  enemies; //arraylist
 
-	void addEnemy(Enemy e);
-	void removeEnemy(Enemy e);
+	//rooms
+	void addAdjacentRoom(Direction d, Room* r);
+	void addCollapsedRoom(Room* r);
+	void clearRooms();
 
-	void addNeighbor(std::string s, Room* r);
-	void removeNeighbor(std::string s);
+	std::map<Direction, Room*> getAdjacentRooms() {
+		return adjacentRooms;
+	}
+	std::map<Direction, Room*> getCollapsedRooms() {
+		return collapsedRooms;
+	}
+
+
+
+	//inside rooms
+	void addEnemy(Enemy* e);
+
+
+private:
+	//vars
+	bool visited = false;
+
+	//rooms
+	std::map<Direction, Room*> adjacentRooms;
+	std::map<Direction, Room*> collapsedRooms;
+	std::map<Direction, Room*> oldRooms;
+
+	//inside rooms
+	std::vector<Unit*>  enemies; //arraylist
+
+	
 
 };
